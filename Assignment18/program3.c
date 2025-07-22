53831e538161701fdd648d28722a6b863df86ef0
Akshay12-04 <akshaypatil86377@gmail.com>
2025-07-22 19:27:07 +0530

    Update :-  22/07/25

---
+++ Assignment18/program3.c
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//File name :   program3.c
+//Description : Accept N numbers from user and display all such elements which are even and divisible by 5.
+//Author :      Akshay Patil             
+//Date :        11-07-25
+//Input :       N : 6
+//              Elements :85 66 3 80 93 88
+//Output :      80
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include <stdlib.h>
+
+void Display (int Arr [],int iLength)
+{
+    int iCnt =0;
+
+    for (iCnt = 0 ; iCnt < iLength ;iCnt++)
+    {
+        if ((Arr[iCnt]) % 5 == 0 && (Arr[iCnt]) % 2 == 0)
+        {
+            printf("%d\t ",Arr[iCnt]);
+        }
+    }
+
+}
+
+int main ()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int *p = NULL;
+
+    printf ("Enter the no of elements ");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc (iSize * sizeof (int));
+
+    if (p == NULL)
+    {
+        printf ("unable to allocate memmory");
+        return -1;
+    }
+
+    printf("enter %d elements \n",iSize);
+
+    for (iCnt = 0; iCnt < iSize;iCnt++)
+    {
+        printf("Enter the element %d :",iCnt+1);
+        scanf ("%d",&p[iCnt]);
+    }
+
+    Display(p ,iSize);
+
+    free(p);
+
+    return 0;
+}
